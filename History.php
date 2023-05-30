b4fdd192055015ce297a572657cd76ea599207ee
Rayyanafie <rayyanafie@gmail.com>
2023-05-30 21:15:54 +0700

    Home Landing History

---
+++ History.php
@@ -0,0 +1,162 @@
+<?php
+include('conn.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../FP/Asset/history.css">
+    <link href="https://fonts.googleapis.com/css?family=Gelasio" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Gabriela" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Newsreader" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>History</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <ul>
+                <li>
+                    <input type="text" name="search" id="search" placeholder="Search">
+                </li>
+                <li>
+                    <select name="bulan" id="bulan">
+                        <option value="1">Januari</option>
+                        <option value="2">Februari</option>
+                        <option value="3">Maret</option>
+                    </select>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../FP/Asset/home (3) 1.png" alt="" class="Icon">
+                        <span class="Description">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../FP/Asset/writing 1.png" alt="" class="Icon">
+                        <span class="Description">Description</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../FP/Asset/notification (1) 1.png" alt="" class="Icon">
+                        <span class="Description">Reminder</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo "History.php"; ?>">
+                        <img src="../FP/Asset/sand-watch 1.png" alt="" class="Icon">
+                        <span class="Description">History</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../FP/Asset/Star 2.png" alt="" class="Icon">
+                        <span class="Description">Priority</span>
+                    </a>
+                </li>
+                <li class="logout">
+                    <a href="#">
+                        <div class="LogOut">Log Out</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="main">
+            <div class="garis">
+                <div class="garis1"></div>
+                <div class="garis2"></div>
+            </div>
+            <div>
+                <div class="header">
+                    <div class="dekor">
+                    </div>
+                    <h1>History</h1>
+                    <div class="dekor">
+                        <div class="lingkaran"></div>
+                        <div class="garis4"></div>
+                    </div>
+                </div>
+                <div class="info">
+                    <table>
+                        <thead>
+                            <th>Judul</th>
+                            <th>Deskrpsi</th>
+                            <th>Deadline</th>
+                            <th>Notes</th>
+                        </thead>
+                        <?php
+                        //proses menampilkan data dari database:
+                        //siapkan query SQL
+                        $query = "SELECT * FROM manajementugas.history";
+                        $result = mysqli_query(connection(), $query);
+                        ?>
+
+                        <?php while ($data = mysqli_fetch_array($result)): ?>
+                            <tr>
+                                <td>
+                                    <?php echo $data['ID_History']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $data['ID_Tugas']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $data['Tanggal']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $data['Catatan']; ?>
+                                </td>
+                            </tr>
+                        <?php endwhile ?>
+
+
+                    </table>
+                </div>
+
+                <div class="garis" style="position: fixed; bottom: 0;">
+                    <div class="garis2"></div>
+                    <div class="garis1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+
+    <div class="popup">
+        <div class="popup-content">
+            <span class="close-btn">&times;</span>
+            <h2>Detail</h2>
+            <p id="popup-description"></p>
+        </div>
+    </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var popup = document.querySelector('.popup');
+            var popupDescription = document.getElementById('popup-description');
+
+            var tableCells = document.querySelectorAll('td');
+
+            tableCells.forEach(function (cell) {
+                cell.addEventListener('click', function () {
+                    var description = this.textContent;
+                    popupDescription.textContent = description;
+                    popup.style.display = 'block';
+                });
+            });
+
+            var closeBtn = document.querySelector('.close-btn');
+            closeBtn.addEventListener('click', function () {
+                popup.style.display = 'none';
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ Landing.php
@@ -0,0 +1,129 @@
+<?php
+
+include 'conn.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+if (isset($_SESSION['Username'])) {
+  header("Location: loginsucces.php");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Retrieve form data
+  $email = $_POST['Email'];
+  $password = $_POST['Password'];
+
+  $query = "SELECT * FROM pengguna WHERE Email = '$email' AND Password = '$password'";
+  $result = mysqli_query(connection(), $query);
+
+  if ($result && mysqli_num_rows($result) > 0) {
+    // Successful login
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['Username'] = $row['Username'];
+    header("Location: home.php");
+    exit;
+  } else {
+    // Incorrect username or password
+    echo "<script>alert('Incorrect username or password.');</script>";
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Notify</title>
+  <link href="Asset/landing.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Gabriela" rel="stylesheet" />
+  <style>
+    #login-page {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #f1f1f1;
+      opacity: 0;
+      animation: slide-up 3s ease-in-out 1.5s forwards;
+    }
+
+    @keyframes slide-up {
+      0% {
+        transform: translateY(100%);
+        opacity: 0;
+      }
+
+      100% {
+        transform: translateY(0%);
+        opacity: 1;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <div id="landing-page">
+    <div class="lingkaran">
+      <span class="circle top-right"></span>
+      <span class="circle bottom-left"></span>
+    </div>
+    <div class="container">
+      <div class="box">
+        <div class="gambar-1"><img src="Asset/gambar1.png" alt="gambar-1" /></div>
+        <div class="judul">
+          <h1 class="notification">Notify</h1>
+          <div class="line"></div>
+        </div>
+        <div class="gambar-2"><img src="Asset/gambar2.png" alt="gambar-2" /></div>
+      </div>
+    </div>
+  </div>
+  <div id="login-page">
+    <div class="lingkaran">
+      <span class="circle top-right"></span>
+      <span class="circle bottom-left"></span>
+    </div>
+    <div class="container">
+      <div class="box">
+        <div class="gambar-1 login"><img src="Asset/gambar1.png" alt="gambar-1" /></div>
+        <div class="judul">
+          <h1 class="notification">Notify</h1>
+          <div class="line"></div>
+          <div class="login-container">
+            <h2>Sign In</h2>
+            <form action="" method="POST">
+              <input type="text" id="Email" name="Email" required placeholder="Email" />
+              <input type="Password" id="Password" name="Password" required placeholder="Password" />
+              <button type="submit">Sign In</button>
+              <a class="login-link" href="signUp.php">Create Account</a>
+              <div class="remember-me">
+                <input type="checkbox" id="remember" name="remember" />
+                <label for="remember">Remember Me</label>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="gambar-2 login"><img src="Asset/gambar2.png" alt="gambar-2" /></div>
+      </div>
+    </div>
+  </div>
+</body>
+<script>
+  setTimeout(function () {
+    document.getElementById("landing-page").style.display = "none";
+    document.getElementById("login-page").style.display = "flex";
+  }, 2000);
+</script>
+
+</html>
